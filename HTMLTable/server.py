e5226a2a351131ab70fe3f9e6dc788e591021f42
Svetie <62491496+Svetie@users.noreply.github.com>
2021-07-09 23:18:49 -0400

    Add files via upload

---
+++ HTMLTable/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template  # added render_template!
+app = Flask(__name__)
+
+@app.route('/')
+def render_lists():
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+    length = len(users)
+
+    return render_template("index.html", users = users, length=length)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
